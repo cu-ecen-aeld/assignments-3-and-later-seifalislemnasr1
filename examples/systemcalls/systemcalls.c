4cd3cf6391a0ed6a80fa8d1679dd31f790bba85c
seifalislemnasr1 <157023643+seifalislemnasr1@users.noreply.github.com>
2024-01-28 22:52:14 +0100

    Update systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdarg.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#include "systemcalls.h"  // Assuming you have a header file with declarations for your functions
-
-/**
- * @param cmd the command to execute with system()
- * @return true if the command in @param cmd was executed
- *   successfully using the system() call, false if an error occurred,
- *   either in invocation of the system() call, or if a non-zero return
- *   value was returned by the command issued in @param cmd.
- */
 bool do_system(const char *cmd)
 {
     int stat = system(cmd);
@@ -25,24 +16,12 @@ bool do_system(const char *cmd)
     }
     else
     {
-        printf("Command executed with exit status %d\n", stat);
+        printf("Command executed with exit status %d", stat);
         return true;
     }
+    return true;
 }
 
-/**
- * @param count -The numbers of variables passed to the function. The variables are command to execute.
- *   followed by arguments to pass to the command
- *   Since exec() does not perform path expansion, the command to execute needs
- *   to be an absolute path.
- * @param ... - A list of 1 or more arguments after the @param count argument.
- *   The first is always the full path to the command to execute with execv()
- *   The remaining arguments are a list of arguments to pass to the command in execv()
- * @return true if the command @param ... with arguments @param arguments were executed successfully
- *   using the execv() call, false if an error occurred, either in invocation of the
- *   fork, waitpid, or execv() command, or if a non-zero return value was returned
- *   by the command issued in @param arguments with the specified arguments.
- */
 bool do_exec(int count, ...)
 {
     va_list args;
@@ -64,15 +43,7 @@ bool do_exec(int count, ...)
     }
     else if (son_pid == 0)
     {
-        char *sous[count + 1];
-        for (int i = 0; i < count; i++)
-        {
-            sous[i] = (char *)command[i + 1];
-        }
-        sous[count] = NULL;
-
-        int stat_ex = execv(command[0], sous);
-        if (stat_ex == -1)
+        if (execv(command[0], (char *const *)command) == -1)
         {
             perror("Execv failed");
             exit(EXIT_FAILURE);
@@ -92,11 +63,6 @@ bool do_exec(int count, ...)
     return true;
 }
 
-/**
- * @param outputfile - The full path to the file to write with command output.
- *   This file will be closed at completion of the function call.
- * All other parameters, see do_exec above
- */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
@@ -117,19 +83,14 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     }
 
     pid_t sonpid;
-    switch (sonpid = fork())
+    sonpid = fork();
+    switch (sonpid)
     {
     case -1:
         perror("Error Forking");
         return false;
     case 0:
-        if (dup2(fd, 1) < 0)
-        {
-            perror("Dup2");
-            exit(EXIT_FAILURE);
-        }
-        close(fd);
-
+    {
         char *sous[count + 1];
         for (int i = 0; i < count; i++)
         {
@@ -137,14 +98,21 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         }
         sous[count] = NULL;
 
-        int stat_ex1 = execvp(command[0], sous);
-        if (stat_ex1 == -1)
+        if (dup2(fd, 1) < 0)
+        {
+            perror("Dup2");
+            exit(EXIT_FAILURE);
+        }
+        close(fd);
+
+        if (execvp(command[0], sous) == -1)
         {
             perror("Execvp failed");
             exit(EXIT_FAILURE);
         }
-
+    }
     default:
+    {
         close(fd);
         int status;
         if (waitpid(sonpid, &status, 0) == -1)
@@ -153,18 +121,14 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
             return false;
         }
     }
+    }
 
     va_end(args);
     return true;
 }
 
-
 int main()
 {
-    // Example usage:
-    do_system("ls -l");
-    do_exec(3, "/bin/echo", "Hello", "World");
-    do_exec_redirect("output.txt", 3, "/bin/echo", "Redirected", "Output");
-
+    // You can add test cases or use the functions in your program here
     return 0;
 }
